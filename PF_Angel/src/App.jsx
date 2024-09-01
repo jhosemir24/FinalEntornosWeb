9c87bd9a06abe1dbda50f739050d5787cb24211f
jhosemir24 <144852794+jhosemir24@users.noreply.github.com>
2024-09-01 01:46:13 +0000

    Nuevo doc

---
+++ PF_Angel/src/App.jsx
@@ -0,0 +1,12 @@
+import { Fragment } from "react";
+import Registro from './componentes/Registro'
+
+function App (){
+  return (
+    <>
+    <Registro/>
+    </>
+  );
+}
+
+export default App
---
+++ PF_Angel/src/componentes/Registro.jsx
@@ -0,0 +1,11 @@
+import { Fragment } from "react"
+
+function Registro (){
+    return (
+        <>
+        <h1>Hola mundo</h1>
+        </>
+    );
+}
+
+export default Registro
