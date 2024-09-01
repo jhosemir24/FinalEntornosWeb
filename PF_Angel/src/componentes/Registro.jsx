7724d77dabc66e169209fd0e3ab711cddae00048
jhosemir24 <144852794+jhosemir24@users.noreply.github.com>
2024-09-01 02:57:57 +0000

    cosas

--- PF_Angel/src/componentes/Registro.jsx
+++ PF_Angel/src/componentes/Registro.jsx
@@ -1,11 +1,156 @@
 import { Fragment } from "react"
+import '../componentes/Contactos.css'
+import citas from '../image/contac_citas-virtuales.png'
+import contact from '../image/contac_contact-center.png'
+import portal from '../image/contac_portal-de-ayuda.png'
+import tienda from '../image/contac_tienda.png'
+import web from '../image/contac_web.png'
+import whatsapp from '../image/contac_whatsapp.png'
 
-function Registro (){
+
+function Registro() {
     return (
         <>
-        <h1>Hola mundo</h1>
+            <h1 id="title-glb">CANALES DE ATENCIÓN</h1>
+            <div id="ayuda_glb">
+                <div className="ayuda">
+                    <h2 className="text_r">Nombre*</h2>
+                    <input
+                        className="input_mode"
+                        type="text"
+                        placeholder="Ingrese su nombre completo"
+                    />
+                </div>
+                <div className="ayuda">
+                    <h2 className="text_r">Email*</h2>
+                    <input
+                        className="input_mode"
+                        type="email"
+                        placeholder="Ingrese su correo electronico"
+                    />
+                </div>
+                <div className="ayuda">
+                    <h2 className="text_r">Telefono*</h2>
+                    <input
+                        className="input_mode"
+                        type="tel"
+                        placeholder="Ingrese su Telefono"
+                    />
+                </div>
+                <div className="ayuda">
+                    <h2 className="text_r">Consulta*</h2>
+                    <select className="input_mode">
+                        <option>¿En que le podemos ayudar?</option>
+                        <option>Queja</option>
+                        <option>Reclamacion</option>
+                        <option>Informacion</option>
+                    </select>
+                </div>
+                <div className="ayuda">
+                    <h2 className="text_r">Mensaje*</h2>
+                    <textarea
+                        className="input_mode"
+                        rows={4}
+                        cols={50}
+                        defaultValue={"Ingrese su mensaje"}
+                    />
+                </div>
+                <div className="ayuda">
+                    <h2 className="text_r">Archivo adjunto*</h2>
+                    <input className="input_mode" type="file" />
+                </div>
+                <div className="ayuda" id="ayuda-id">
+                    <button className="input_mode">Enviar</button>
+                    <button className="input_mode">Borrar</button>
+                </div>
+            </div>
+            <div id="contacto-glb">
+                <div>
+                    <div className="contacto-select">
+                        <div>
+                            <img 
+                            className= "giro"
+                            src={tienda}
+                            style={{ transform: `rotate(${rotationAngle}deg)` }}
+                            onClick={handleImageClick}
+                            alt="" />
+                        </div>
+                        <div className="contac-info-2">
+                            <h2>Tiendas</h2>
+                            <h3>Asesoria presencial para tus vacaciones</h3>
+                        </div>
+                        <div>
+                            <a href="">Visitanos</a>
+                        </div>
+                    </div>
+                    <div className="contacto-select">
+                        <div>
+                            <img src={web} alt="" />
+                        </div>
+                        <div className="contac-info-2">
+                            <h2>Web</h2>
+                            <h3>Reserva, cotiza y compra tu proximo viaje</h3>
+                        </div>
+                        <div>
+                            <a href="">Compra</a>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div className="contacto-select">
+                        <div>
+                            <img src={whatsapp} alt="" />
+                        </div>
+                        <div className="contac-info-2">
+                            <h2>WhatsApp</h2>
+                            <h3>Cotiza, compra y planifica todo desde tu celular</h3>
+                        </div>
+                        <div>
+                            <a href="">Escribenos</a>
+                        </div>
+                    </div>
+                    <div className="contacto-select">
+                        <div>
+                            <img src={contact} alt="" />
+                        </div>
+                        <div className="contac-info-2">
+                            <h2>Contac Center</h2>
+                            <h3>Llámanos, resulve tus dudas y planifica tu viaje con nosotros</h3>
+                        </div>
+                        <div>
+                            <a href="">(01) 123-987</a>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div className="contacto-select">
+                        <div>
+                            <img src={citas} alt="" />
+                        </div>
+                        <div className="contac-info-2">
+                            <h2>Citas Virtuales</h2>
+                            <h3>Te asesoramos para planificar tus próximas vacaciones</h3>
+                        </div>
+                        <div>
+                            <a href="">Agenda tu cita</a>
+                        </div>
+                    </div>
+                    <div className="contacto-select">
+                        <div>
+                            <img src={portal} alt="" />
+                        </div>
+                        <div className="contac-info-2">
+                            <h2>Portal de ayuda</h2>
+                            <h3>Solicita cambios, reembolsos o seguimiento de tu caso</h3>
+                        </div>
+                        <div>
+                            <a href="">Ayuda</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     );
 }
 
-export default Registro
+export default Registro;
